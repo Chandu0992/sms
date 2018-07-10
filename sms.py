62bdb02f3ff106d89e160f794001444d7aeeb080
Chandu0992 <30655263+Chandu0992@users.noreply.github.com>
2018-07-10 09:31:10 +0530

    Update sms.py

--- sms.py
+++ sms.py
@@ -10,29 +10,30 @@ def route():
 def messages():
     password = request.args.get('password')
     if(password == '2194_Prabha'):
-        mobiles = request.args.get('mobile')
-        message = request.args.get('message')
-        authkey = "176467AjyjNl6c5aabf935" # Your authentication key.
-        #mobiles = "9705707580,8978098160" # Multiple mobiles numbers separated by comma.
-        #message = "Status Demo" # Your message to send.
-        sender = "MSGIND" # Sender ID,While using route4 sender id should be 6 characters long.
-        route = "4" # Define route
-        # Prepare you post parameters
-        values = {
-                 'authkey' : authkey,
-                 'mobiles' : mobiles,
-                 'message' : message,
-                 'sender' : sender,
-                 'route' : route
-                 }
-        url = "https://control.msg91.com/api/sendhttp.php" # API URL
-        postdata = urllib.parse.urlencode(values) # URL encoding the data here.
-        postdata = postdata.encode('utf-8')
-        req = urllib.request.Request(url,postdata)
-        response = urllib.request.urlopen(req)
-        output = response.read() # Get Response
-        print("hai")
-        return "Message Sent Successfully"
+        while True:
+            mobiles = request.args.get('mobile')
+            message = request.args.get('message')
+            authkey = "176467AjyjNl6c5aabf935" # Your authentication key.
+            #mobiles = "9705707580,8978098160" # Multiple mobiles numbers separated by comma.
+            #message = "Status Demo" # Your message to send.
+            sender = "MSGIND" # Sender ID,While using route4 sender id should be 6 characters long.
+            route = "4" # Define route
+            # Prepare you post parameters
+            values = {
+                     'authkey' : authkey,
+                     'mobiles' : mobiles,
+                     'message' : message,
+                     'sender' : sender,
+                     'route' : route
+                     }
+            url = "https://control.msg91.com/api/sendhttp.php" # API URL
+            postdata = urllib.parse.urlencode(values) # URL encoding the data here.
+            postdata = postdata.encode('utf-8')
+            req = urllib.request.Request(url,postdata)
+            response = urllib.request.urlopen(req)
+            output = response.read() # Get Response
+            print("hai")
+            return "Message Sent Successfully"
     else:
         return "Invalid Password"
 #"successfully send"
